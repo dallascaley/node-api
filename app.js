9c445d765e09bbe9dc1cfa7c83e81dbeff17e135
Dallas Caley <dallascaley@gmail.com>
2015-11-21 04:22:10 +0000

    Testing a git commit on github

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.send('Testing 123!');
 });
 
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('node-api started at http://%s:%s', host, port);
+
 });
